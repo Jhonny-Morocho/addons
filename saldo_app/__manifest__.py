fd82f4328e3683e29d9c6e1ca1b3085ea07421ee
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-03-03 13:11:52 -0500

    MODELOS

---
+++ saldo_app/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name":"SALDO APP",
+    'description': """
+        Aplicacion para ingresar ingresos y egresos
+    """,
+    'author': "Jhonny Morocho",
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        #"security/res_groups.xml,
+        "views/views.xml"
+    ]
+}
---
+++ saldo_app/models/models.py
@@ -0,0 +1,45 @@
+from odoo import fields,models
+
+class Movimiento (models.Model):
+
+    _name = 'sa.movimiento' #sa_movimiento
+    _description = 'Tabla movimiento'
+    ##campos
+    name = fields.Char("Nombre")
+    type_move= fields.Selection(selection=[("ingreso","Ingreso"),("gasto","Gasto")])
+    date = fields.Datetime("Fecha")
+    amount = fields.Float("Monto")
+    receipt_image = fields.Binary("Foto del recibo")
+    ##crera relacion
+    user_id = fields.Many2one('res.user',string='Usuarios')
+    categoria_id = fields.Many2one('sa.category.',string='Categoria')
+    ##movimiento & tags es mucho a mucho
+    #tag_ids = fields.Many2many('sa.tag') # odo hace= sa_movimiento_sa_tag_rel
+    #nombre personalizado y los campos
+    tag_ids = fields.Many2many('sa.tag','sa_mov_sa_tag_rel','mov_id','tag_id')
+class Category(models.Model):
+
+    _name = 'sa.category'
+    _description = 'Tabla categoria'
+    ##campos
+    name = fields.Char("Nombre")
+class Tag(models.Model):
+
+    _name = 'sa.tag'
+    _description = 'Tabla tags'
+    ##campos
+    name = fields.Char("Nombre")
+## agregar un nuevo campo a una tabla existen de odoo ## extender un modelo
+class ResUsers(models.Model):
+
+    _inherit = 'res.users'
+    #one may necesita tener user_id para trabajar 
+
+    movimiento_ids = fields.One2many('sa.movimiento', 'user_id')
+    
+
+
+
+### LA TABLA USUARIO YA EXISTE POR ENDE OCUPAREMOS LA QUE VIENE EN ODOO ##
+
+
