cfa3813262662999a2bc846583baacbed43416dd
Jhonny-Morocho <37280267+Jhonny-Morocho@users.noreply.github.com>
2022-03-05 16:11:37 -0500

    termiando proyecto

--- fichas_autos/models/models.py
+++ fichas_autos/models/models.py
@@ -22,8 +22,17 @@ class Ficha (models.Model):
     vehiculo_image = fields.Binary(string="Foto del vehiculo",required=True)
     
     ##realcionar modelos con la tabla existente usuarios many2One tabla usuario con movimiento
-    user_id = fields.Many2one('res.users',string='Usuario')
-
+    user_id = fields.Many2one('res.users',string='Usuario',default=lambda self:self.env.user.id)
+    ##def onchange(self):
+      ##  return{
+        ##    "type":"ir.actions.act_window",
+         #   "name":"Proveedor "+self.name,
+          #  "res_model":"res.users",
+           # "views":[[False,"tree"]],
+            #"target"="self",
+            #"domain":[["ficha_id","=",self.id]]
+        #}
+    
 class ResUser(models.Model):
     _inherit = "res.users"
     ficha_id = fields.One2many('ft.ficha', 'user_id')
